28957c6859d26f03798927dce445fe24cd3d0029
Graham Knop <haarg@haarg.org>
2024-10-10 21:03:23 +0200

    update push-tags to keep local tags updated

--- push-tags.pl
+++ push-tags.pl
@@ -2,8 +2,25 @@
 use v5.36;
 
 my $version = shift;
-my @parts = $version =~ /\Av(\d+)\.(\d+)\.(\d+)\z/;
+my @parts = $version =~ /\Av(\d+)\.(\d+)\.(\d+)(-TRIAL)?\z/;
+
+die "Invalid version $version!\n"
+  if !@parts;
+
+exit
+  if !$parts[3];
+
+my $tag_ref = `git rev-parse --verify -q $version`;
+chomp $tag_ref;
+if (!$tag_ref) {
+  die "No tag for version $version!\n";
+}
 
 my @short_versions = ("v$parts[0]", "v$parts[0]\.$parts[1]");
 
-system 'git', 'push', 'origin', '-f', map "$version:$_", @short_versions;
+for my $v (@short_versions) {
+  system 'git', 'update-ref', '-d', "refs/tags/$v"
+  system 'git', 'tag', $v, $version;
+}
+
+system 'git', 'push', 'origin', '-f', @short_versions;
