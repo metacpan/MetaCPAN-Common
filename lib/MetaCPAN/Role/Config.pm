9336b874aa1fc262adcad56664c1700a132f247b
Graham Knop <haarg@haarg.org>
2024-10-10 20:52:16 +0200

    config: automatically calculate path when possible
    When called from a module in a lib/ directory, automatically set path to
    the appropriate root directory.
    
    Since path can be calculated, allow passing just the name as a single
    parameter.

--- lib/MetaCPAN/Role/Config.pm
+++ lib/MetaCPAN/Role/Config.pm
@@ -7,11 +7,27 @@ our $VERSION = 'v1.0.1';
 { use Moo::Role; }
 
 use Config::ZOMG;
-use File::Spec::Functions qw(rel2abs);
+use File::Spec::Functions qw(rel2abs canonpath);
 use Carp                  qw(croak confess);
 use MetaCPAN::Common      qw(visit dpath);
 
 use namespace::clean;
+
+sub _find_caller {
+  my $self = shift;
+  my $class = ref $self || $self;
+  my $c = 0;
+  while (my @c = caller($c++)) {
+    next
+      if $c[0] eq __PACKAGE__
+        || $c[0]->isa($class)
+        || $c[0]->isa('Class::MOP::Mixin')
+        || $c[0] =~ /\AEval::Closure::Sandbox_/;
+
+    return @c;
+  }
+  return;
+}
 
 has name => (
   is       => 'ro',
@@ -20,7 +36,36 @@ has name => (
 has path => (
   is       => 'ro',
   coerce   => sub { defined $_[0] ? rel2abs( $_[0] ) : undef },
+  default => sub {
+    my $self = shift;
+    my ($package, $file) = $self->_find_caller;
+
+    return undef
+      if !$file;
+
+    my $lib_path
+      = canonpath( join '/', '', 'lib', split /::/, $package . '.pm' );
+    my $fullpath = canonpath( rel2abs($file) );
+    if ( -e $file && $fullpath =~ s{\Q$lib_path\E\z}{} ) {
+      $fullpath ||= '/';
+      return $fullpath;
+    }
+    return undef;
+  },
 );
+
+around BUILDARGS => sub {
+  my ($orig, $self) = (shift, shift);
+  if (@_ == 1) {
+    my ($name) = @_;
+    if (!ref $name) {
+      return {
+        name => $name,
+      };
+    }
+  }
+  $self->$orig(@_);
+};
 
 has config => (
   is      => 'ro',
